9171a0be241ae136a25739ec0bddf991e0a525cf
Maisam Alaa <52468869+maisamjuma@users.noreply.github.com>
2025-08-26 13:13:54 +0300

    added chart

--- Admin_Dashboard-main/Admin_Dashboard-main/src/main.ts
+++ Admin_Dashboard-main/Admin_Dashboard-main/src/main.ts
@@ -3,7 +3,14 @@ import { AppComponent } from './app.component';  // <-- note the double app/app 
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
+ import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { ChartsComponent } from './app/Pages/charts/charts.component';
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes),   provideHttpClient()]
 }).catch(err => console.error(err));
+
+
+bootstrapApplication(ChartsComponent, {
+  providers: [provideCharts(withDefaultRegisterables())],
+}).catch((err) => console.error(err));
